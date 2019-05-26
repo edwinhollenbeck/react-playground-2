f173cf1af6f19e8ecf48f715a3bb7c1e9f7d548d
Edwin Hollenbeck <ehollenbeck@apple.com>
2019-05-26 15:51:26 -0700

    Completed content for checkpoint 10.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import React from 'react';
 import TheDate from './state/TheDate';
 import Counter from './state/Counter';
+import Tabs from './state/Tabs';
+
+const tabsProp = [
+  { name: 'First tab',
+    content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam exercitationem quos consectetur expedita consequatur. Fugit, sapiente aspernatur corporis velit, dolor eum reprehenderit provident ipsam, maiores incidunt repellat! Facilis, neque doloremque.' },
+  { name: 'Second tab',
+    content: 'Laboriosam exercitationem quos consectetur expedita consequatur. Fugit, sapiente aspernatur corporis velit, dolor eum reprehenderit provident ipsam, maiores incidunt repellat! Facilis, neque doloremque. Lorem ipsum dolor sit amet consectetur adipisicing elit.' },
+  { name: 'Third tab',
+    content: 'Fugit, sapiente aspernatur corporis velit, dolor eum reprehenderit provident ipsam, maiores incidunt repellat! Facilis, neque doloremque. Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam exercitationem quos consectetur expedita consequatur.' },
+];
 
 function App() {
   return (
     <main className='App'>
-      <TheDate />
-      <Counter count={123} />
+      {/*<TheDate />
+      <Counter count={123} />*/}
+      <Tabs tabs={tabsProp} />
     </main>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-//import App from './App';
+import App from './App';
 import './index.css';
-//import HelloWorld from './state-drills/HelloWorld';
-//import Bomb from './state-drills/Bomb';
+import HelloWorld from './state-drills/HelloWorld';
+import Bomb from './state-drills/Bomb';
 import RouletteGun from './state-drills/RouletteGun';
 
-ReactDOM.render(<RouletteGun bulletInChamber={8} />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
+//ReactDOM.render(<HelloWorld />, document.getElementById('root'));
+//ReactDOM.render(<Bomb />, document.getElementById('root'));
+//ReactDOM.render(<RouletteGun bulletInChamber={8} />, document.getElementById('root'));
